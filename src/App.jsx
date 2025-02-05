f2e617a82d81db1d0b669c0a5808436adb3120f6
TusharSin810 <tsinghal666@gmail.com>
2025-02-05 16:37:41 +0530

    Removed React

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold bg-slate-200 flex w-screen h-screen items-center justify-center">resuRabbit</h1>
-    </div>
-  )
-}
-
-export default App
