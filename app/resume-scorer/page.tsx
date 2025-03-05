944843dde40f44bf332ccafa6cc3f8b8af57b619
Vidip Singh <112854574+vidipsingh@users.noreply.github.com>
2025-03-05 18:25:12 +0530

    Update ATS Resume Checker Feature

---
+++ app/resume-scorer/page.tsx
@@ -0,0 +1,9 @@
+import ResumeScorer from '../../components/ResumeScorer';
+
+export default function ResumeScorerPage() {
+    return (
+      <div className="min-h-screen bg-gray-950 flex items-center justify-center p-4">
+        <ResumeScorer />
+      </div>
+    );
+}
