97a302df67149b112f570643334d835cc7a196d8
Vidip Singh <112854574+vidipsingh@users.noreply.github.com>
2025-03-05 18:27:39 +0530

    Update ATS Resume Builder` Feature

---
+++ app/resume-builder/page.tsx
@@ -0,0 +1,12 @@
+import ResumeBuilder from '@/components/ResumeBuilder'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen bg-gray-950 flex items-center justify-center p-4'>
+      <ResumeBuilder />
+    </div>
+  )
+}
+
+export default page
